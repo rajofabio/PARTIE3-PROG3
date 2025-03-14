a19510589a25518d12ac3f433d20ad0f4a600da0
Ryan Andriamahery <ryan@hei.school>
2025-03-14 20:09:54 +0300

    initial commit

---
+++ src/main/java/edu/restaurant/app/RestaurantApplication.java
@@ -0,0 +1,10 @@
+package edu.restaurant.app;
+
+import java.util.logging.Logger;
+
+public class RestaurantApplication {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("RestaurantApplication");
+        logger.info("Starting application");
+    }
+}
