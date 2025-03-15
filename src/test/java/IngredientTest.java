0d2241720aa693a785d3e37820699c70ce840bbb
Ryan Andriamahery <ryan@hei.school>
2025-03-15 11:32:56 +0300

    test: IngredientTest::get_available_quantity

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,23 @@
+import edu.restaurant.app.dao.entity.Ingredient;
+import edu.restaurant.app.dao.operations.IngredientCrudOperations;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IngredientTest {
+    IngredientCrudOperations subject = new IngredientCrudOperations();
+
+    @Test
+    void oeuf_get_available_quantity() {
+        Ingredient oeuf = subject.findById(1L);
+
+        assertEquals(80.0, oeuf.getAvailableQuantity());
+    }
+
+    @Test
+    void pain_get_available_quantity() {
+        Ingredient pain = subject.findById(4L);
+
+        assertEquals(30.0, pain.getAvailableQuantity());
+    }
+}
